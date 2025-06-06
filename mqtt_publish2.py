083feafd3ce51fa57056c851ec646e4728bb34a0
Carlos De Grandchant <carlosgrandchant@hotmail.com>
2025-06-06 09:39:20 -0400

    First commit.

---
+++ mqtt_publish2.py
@@ -0,0 +1,14 @@
+import paho.mqtt.client as mqtt
+from random import randint
+import time
+
+
+mqttBroker = 'mqtt.eclipseprojects.io'
+client = mqtt.Client()
+client.connect(mqttBroker)
+
+while True:
+    randomNumber = randint(1, 200)
+    client.publish("POS", randomNumber)
+    print(f"SUCCESS: \nPublished 'TEMPERATURE' : {randomNumber}")
+    time.sleep(10)
---
+++ mqtt_subscribe.py
@@ -0,0 +1,28 @@
+import paho.mqtt.client as mqtt
+from random import randint, getrandbits
+import time
+
+
+class Smartphone:
+    def __init__(self):
+        self.broker = 'mqtt.eclipseprojects.io'
+        self.client = mqtt.Client()
+        self.client.connect(self.broker)
+
+
+def on_message(client, userdata, message):
+    print(f"Received Mesage: \n{str(message.payload)}")
+    
+    
+    
+
+while True:
+    s = Smartphone()
+    s.client.loop_start()
+    s.client.subscribe("TEMPERATUREspc")
+    s.client.on_message = on_message
+    time.sleep(30)
+
+s.client.loop_stop()
+    
+    
